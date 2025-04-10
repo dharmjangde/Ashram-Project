33fb03e26db7c2fbe0bbc91aa5edd661731306b5
Nawalnayak <nawalnayak143@gmail.com>
2025-04-10 17:02:58 +0530

    hello

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
-import { BrowserRouter , Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
- import About from "./pages/about";
+import About from "./pages/about";
 import Contact from "./pages/contact";
 import Navbar from "./common/components/header/Navbar";
 import Footer from "./common/components/footer/footer"; // Ensure 'Footer' is correctly capitalized
 
-
 function App() {
   return (
     <>
-     <BrowserRouter>
+      <BrowserRouter>
         <Navbar />
-         <Routes> 
-          <Route path="/" element={<Home />} /> 
-           <Route path="/about" element={<About />} />  
-           <Route path="/contact" element={<Contact />}/>
-        </Routes> 
+        <Routes>
+          <Route path="/" element={<Home />} />{" "}
+          <Route path="/about" element={<About />} />{" "}
+          <Route path="/contact" element={<Contact />} />{" "}
+        </Routes>{" "}
         <Footer />
-     </BrowserRouter>
+      </BrowserRouter>{" "}
     </>
-
   );
 }
 
 export default App;
+//hello jeee
